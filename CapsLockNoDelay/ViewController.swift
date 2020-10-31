5d3d1cb78df35e52d0729162f5409f64fddcbc56
Guy Kaplan <gkpln3@gmail.com>
2020-10-31 13:17:04 +0200

    Initial Commit

---
+++ CapsLockNoDelay/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CapsLockNoDelay
+//
+//  Created by Guy Kaplan on 31/10/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
