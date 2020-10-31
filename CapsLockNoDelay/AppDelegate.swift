6fd8a26df061ce1e7325da03fab396571c25a6bd
Guy Kaplan <guyk@bindecy.com>
2020-10-31 13:17:04 +0200

    Initial Commit

---
+++ CapsLockNoDelay/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CapsLockNoDelay
+//
+//  Created by Guy Kaplan on 31/10/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
