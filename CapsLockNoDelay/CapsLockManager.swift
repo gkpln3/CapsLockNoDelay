495a237ac54f820861028785533570ed89019e23
Guy Kaplan <guyk@bindecy.com>
2020-10-31 14:38:10 +0200

    Moved file

--- CapsLockNoDelay/CapsLockManager.swift
+++ CapsLockNoDelay/CapsLockManager.swift
@@ -6,22 +6,60 @@
 //
 
 import Foundation
+import Cocoa
 
 class CapsLockManager {
-    var ioConnect: io_connect_t = .init(0)
-    let ioService = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching(kIOHIDSystemClass))
-    IOServiceOpen(ioService, mach_task_self_, UInt32(kIOHIDParamConnectType), &ioConnect)
+    var currentState = false
 
-    var modifierLockState = false
-    IOHIDGetModifierLockState(ioConnect, Int32(kIOHIDCapsLockState), &modifierLockState)
+    func requestAccess() -> Bool {
+        let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String : true]
+        let accessEnabled = AXIsProcessTrustedWithOptions(options)
 
-    modifierLockState.toggle()
-    //        modifierLockState = state
-    IOHIDSetModifierLockState(ioConnect, Int32(kIOHIDCapsLockState), modifierLockState)
+        if !accessEnabled {
+            print("Access Not Enabled")
+            return false
+        }
+        return true
+    }
 
-    IOServiceClose(ioConnect)
-
-    init() {
+    func registerEventListener() {
+        currentState = self.getCapsLockState()
         
+        NSEvent.addGlobalMonitorForEvents(matching: [.keyUp, .systemDefined]) { (event) in
+            if (event.type != .systemDefined) {
+                return
+            }
+
+            if (event.subtype.rawValue == 211) {
+                if (event.data1 == 0) {
+                    self.currentState = self.getCapsLockState()
+                    print("Got state \(self.currentState)")
+                }
+                else {
+                    self.toggleCapsLock(!self.currentState)
+                    print("setting state \(!self.currentState)")
+                }
+            }
+        }
+    }
+
+    func toggleCapsLock(_ state: Bool) {
+        var ioConnect: io_connect_t = .init(0)
+        let ioService = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching(kIOHIDSystemClass))
+        IOServiceOpen(ioService, mach_task_self_, UInt32(kIOHIDParamConnectType), &ioConnect)
+        IOHIDSetModifierLockState(ioConnect, Int32(kIOHIDCapsLockState), state)
+        IOServiceClose(ioConnect)
+    }
+
+    func getCapsLockState() -> Bool {
+        var ioConnect: io_connect_t = .init(0)
+        let ioService = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching(kIOHIDSystemClass))
+        IOServiceOpen(ioService, mach_task_self_, UInt32(kIOHIDParamConnectType), &ioConnect)
+
+        var modifierLockState = false
+        IOHIDGetModifierLockState(ioConnect, Int32(kIOHIDCapsLockState), &modifierLockState)
+
+        IOServiceClose(ioConnect)
+        return modifierLockState;
     }
 }
